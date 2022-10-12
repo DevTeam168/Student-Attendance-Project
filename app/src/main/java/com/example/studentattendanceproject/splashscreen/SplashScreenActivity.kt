d603b12019bd3f1b7040d3086d7a7f42d268877b
Khem Rothana <khemrothana99@gmail.com>
2022-10-12 23:46:04 +0700

    update splash screen

--- app/src/main/java/com/example/studentattendanceproject/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/studentattendanceproject/splashscreen/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.studentattendanceproject.splashscreen
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.studentattendanceproject.R
 import com.example.studentattendanceproject.databinding.SplashScreenActivityBinding
 
@@ -12,6 +13,8 @@ class SplashScreenActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.splash_screen_activity)
+        mBinding?.ivSplashScreen?.let { Glide.with(this).load(R.mipmap.astronaut).into(it) }
+
     }
 
 }
