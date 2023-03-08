fbd229eb7c3ec282cbc8d0698689013a21d2fe97
Rothana <khemrothana99@gmail.com>
2023-03-08 13:49:25 +0700

    Update

--- app/src/main/java/com/example/studentattendanceproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/studentattendanceproject/activity/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.studentattendanceproject.activity
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.studentattendanceproject.R
@@ -20,15 +17,15 @@ import com.example.studentattendanceproject.helper.Constants.BottomAppBarType.Co
 import com.example.studentattendanceproject.helper.Constants.BottomAppBarType.Companion.REPORT
 
 class MainActivity : AppCompatActivity() {
-    private var homeFragment: Fragment ?= null
-    private var attendanceFragment: Fragment ?= null
-    private var reportFragment: Fragment ?= null
-    private var profileFragment: Fragment ?= null
-    private var mBinding:ActivityMainBinding ?= null
+    private var homeFragment: Fragment? = null
+    private var attendanceFragment: Fragment? = null
+    private var reportFragment: Fragment? = null
+    private var profileFragment: Fragment? = null
+    private var mBinding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initialize()
         initListener()
     }
@@ -47,9 +44,10 @@ class MainActivity : AppCompatActivity() {
             bottomNavBar.apply {
                 cslHome.setOnClickListener {
                     mBinding?.let {
-                        BottomAppBar.selectedState(HOME,it)
+                        BottomAppBar.selectedState(HOME, it)
                         homeFragment?.let { it1 ->
-                            supportFragmentManager.beginTransaction().replace(R.id.main_fragment,it1).commit()
+                            supportFragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment, it1).commit()
                         }
                     }
                 }
@@ -58,33 +56,34 @@ class MainActivity : AppCompatActivity() {
 
                 cslAttendance.setOnClickListener {
                     mBinding?.let {
-                        BottomAppBar.selectedState(ATTENDANCE,it)
+                        BottomAppBar.selectedState(ATTENDANCE, it)
                         attendanceFragment?.let { it1 ->
-                            supportFragmentManager.beginTransaction().replace(R.id.main_fragment,it1).commit() }
+                            supportFragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment, it1).commit()
+                        }
                     }
                 }
 
                 cslReport.setOnClickListener {
                     mBinding?.let {
-                        BottomAppBar.selectedState(REPORT,it)
+                        BottomAppBar.selectedState(REPORT, it)
                         reportFragment?.let { it1 ->
-                            supportFragmentManager.beginTransaction().replace(R.id.main_fragment,it1).commit()
+                            supportFragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment, it1).commit()
                         }
                     }
                 }
 
                 cslProfile.setOnClickListener {
                     mBinding?.let {
-                        BottomAppBar.selectedState(PROFILE,it)
+                        BottomAppBar.selectedState(PROFILE, it)
                         profileFragment?.let { it1 ->
-                            supportFragmentManager.beginTransaction().replace(R.id.main_fragment,it1).commit()
+                            supportFragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment, it1).commit()
                         }
                     }
                 }
             }
-
         }
-
-
     }
 }
--- app/src/main/java/com/example/studentattendanceproject/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/studentattendanceproject/splashscreen/SplashScreenActivity.kt
@@ -4,10 +4,14 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.example.studentattendanceproject.R
+import com.example.studentattendanceproject.activity.MainActivity
 import com.example.studentattendanceproject.activity.SignInSignUpActivity
 import com.example.studentattendanceproject.databinding.SplashScreenActivityBinding
 import kotlinx.coroutines.delay
@@ -26,12 +30,30 @@ class SplashScreenActivity: AppCompatActivity() {
             delay(2000)
             startActivity(Intent(this@SplashScreenActivity, SignInSignUpActivity::class.java))
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            createShortcut()
             finishAffinity()
         }
 
     }
 
     override fun onBackPressed() {
+    }
+
+    private fun createShortcut() {
+        val listShortcut = arrayListOf(getString(R.string.sign_in), getString(R.string.sign_up), getString(R.string.attendance))
+        ShortcutManagerCompat.removeAllDynamicShortcuts(this)
+        for (title in listShortcut) {
+            val intent  = Intent(this, MainActivity::class.java)
+            intent.action = Intent.ACTION_VIEW
+            startActivity(intent)
+            val shortcut = ShortcutInfoCompat.Builder(this, title)
+                .setIntent(intent)
+                .setShortLabel(title)
+                .setIcon(IconCompat.createWithResource(this, R.drawable.home))
+                .build()
+            ShortcutManagerCompat.addDynamicShortcuts(this, arrayListOf(shortcut))
+        }
+
     }
 
 }
